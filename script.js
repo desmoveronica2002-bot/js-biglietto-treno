c4066e8201a4712694db89a97e7defc667f8617b
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-02 16:16:25 +0100

    set the variable

---
+++ script.js
@@ -0,0 +1,17 @@
+function mostraPrezzo() {
+
+const km = document.getElementById("km");
+const eta = document.getElementById("eta");
+
+if ( eta < 18 ) {
+
+}
+
+else if ( eta >65 ) {
+
+}
+ else {
+    
+ }
+
+}
