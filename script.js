82fd954b5343c451e5b07054595372ef61671809
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-03 15:26:15 +0100

    add prompt

--- script.js
+++ script.js
@@ -2,29 +2,32 @@
 
 //const km = parseFloat(document.getElementById("km").value);
 //const eta = parseInt(document.getElementById("eta").value);
-const km = 100;
-const eta = 10;
+
+let km = prompt('km');
+let eta = prompt('eta');
 
 let prezzo = km * 0.21;
 
 if ( eta < 18 ) {
 
     prezzo = prezzo - ( prezzo * 0.20 );
+    console.log("Prezzo: €" + prezzo.toFixed(2));
 
 }
 
 else if ( eta > 65 ) {
 
-    prezzo = prezzo - (prezzo * 0.40 ); 
+    prezzo = prezzo - (prezzo * 0.40 );
+    console.log("Prezzo: €" + prezzo.toFixed(2)); 
+
 
 }
  else {
 
-    console.log("Prezzo finale:", prezzo, "€");
+    console.log("Prezzo: €" + prezzo.toFixed(2));
 
  }
 
- console.log("Prezzo: €" + prezzo.toFixed(2));
 
  //document.getElementById("prezzo").innerText = "Prezzo: € " + prezzo.toFixed(2); // toFixed limita il numero dei decimali
 
